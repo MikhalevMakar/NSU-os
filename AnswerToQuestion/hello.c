3ea134de982f04ca3be0a4f6f374c24eb2998597
MikhalevMakar <mikhalevmakar@mail.ru>
2023-02-20 13:00:01 +0700

    add folder AnswerToQuestion

---
+++ AnswerToQuestion/hello.c
@@ -0,0 +1,11 @@
+#include "hello_from_static_lib.h"
+
+void printHello() {
+    printf("Hello world\n");
+}
+
+int main() {
+
+    printHello();
+    return 0;
+}
---
+++ AnswerToQuestion/hello_from_static_lib.c
@@ -0,0 +1,5 @@
+#include "hello_from_static_lib.h"
+
+void hello_from_static_lib() {
+    printf("hello_from_static_lib\n");
+}
