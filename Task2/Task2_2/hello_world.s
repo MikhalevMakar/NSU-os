dc62f662dc8858fe1afc9baf2623c9b2b0d44f7e
MikhalevMakar <mikhalevmakar@mail.ru>
2023-03-27 08:55:51 +0700

    add return value

--- Task2/Task2_2/hello_world.s
+++ Task2/Task2_2/hello_world.s
@@ -7,13 +7,12 @@ msg:
 	.global _start
 
 _start:
-	movq $1, %rax
-	movq $1, %rdi
-	movq $msg, %rsi
+	movq $4, %rax
+	movq $1, %rbx
+	movq $msg, %rcx
 	movq $len, %rdx
-	syscall
+	int $0x80
 
-
-	movq $60, %rax
+	movq $1, %rax
 	xorq %rdi, %rdi
-	syscall
+	int $0x80
